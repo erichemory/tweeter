0137a2f7ecc47d1b86377231bd21c70324be05e8
Erich <erichemory@gmail.com>
2015-12-08 11:06:08 -0600

    continued work all over the app

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -4,11 +4,17 @@ class EpicenterController < ApplicationController
   	@tweets = Tweet.all
 
   	@tweets.each do |tweet|
-  		current_user.following.each do |f|
-  			if tweet.user_id == f
-  				@following_tweets.push(tweet)
+  		if current_user.following.include?(tweet.user.id)
+  			@following_tweets.push(tweet)
   			end
   		end
+
+  		@follower_count = 0
+  		@users = User.all
+  		@users.each do |user|
+  			if user.following.include?(current_user.id)
+  				@follower_count =+ 1
+  			end
   	end
   end
 
@@ -20,11 +26,15 @@ class EpicenterController < ApplicationController
   	@user = User.find(params[:follow_id])
   	current_user.following.push(params[:follow_id].to_i)
   	current_user.save
+
+  	redirect_to user_profile_path(id: @user.id)
   end
 
   def unfollow
   	@user = User.find(params[:unfollow_id])
   	current_user.following.delete(params[:unfollow_id].to_i)
   	current_user.save
+
+  	redirect_to user_profile_path(id: @user.id)
   end
 end
